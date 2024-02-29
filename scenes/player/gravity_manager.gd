6d5f60d74435166c83ef020cd7522da74bb75328
Annabelle Lewis <defuego424242@gmail.com>
2024-02-29 14:36:40 -0800

    Player gravity is now defined by area data stack

--- scenes/player/gravity_manager.gd
+++ scenes/player/gravity_manager.gd
@@ -2,9 +2,20 @@ class_name GravityManager
 extends Node
 
 
+@export var gravity_influences : Array[GravityAreaData]
+
+
 func get_gravity_direction() -> Vector2:
-    return Vector2(0, -1)
+    return gravity_influences.front().up_direction
 
 
 func get_graivty_scale() -> float:
-    return 1.0
+    return gravity_influences.front().gravity_scale
+
+
+func add_influince(influence : GravityAreaData) -> void:
+    gravity_influences.push_front(influence)
+
+
+func remove_influince(influince : GravityAreaData) -> void:
+    gravity_influences.erase(influince)
