f7fb1cc1a02ac680fc07339cefed2fefaf59e894
Annabelle Lewis <defuego424242@gmail.com>
2024-02-29 09:48:38 -0800

    Set up App state management code

---
+++ scenes/app_states/gameplay_state/gameplay_app_state.gd
@@ -0,0 +1,15 @@
+class_name GameplayAppState
+extends Node
+
+
+## GameplayAppState, Controlls the gameplay half of the game
+##
+## Handles level advancing, resetting, and returning to the main menu when needed
+
+
+func enter_state(params : Dictionary) -> void:
+    pass
+
+
+func exit_state() -> void:
+    pass
