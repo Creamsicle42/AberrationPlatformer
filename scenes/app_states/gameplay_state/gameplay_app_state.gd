20361d9ad73012b39c20f30f662519d9cee2cf32
Annabelle Lewis <defuego424242@gmail.com>
2024-02-29 10:25:00 -0800

    Set up reseting and advnacing of level

--- scenes/app_states/gameplay_state/gameplay_app_state.gd
+++ scenes/app_states/gameplay_state/gameplay_app_state.gd
@@ -18,3 +18,21 @@ func enter_state(params : Dictionary) -> void:
 
 func exit_state() -> void:
     pass
+
+
+func player_reached_end_of_game() -> void:
+    pass
+
+
+func _on_event_bus_player_killed() -> void:
+    level_manager.instance_current_level()
+
+
+func _on_event_bus_end_goal_reached() -> void:
+    var more_levels_exist = level_manager.advance_level()
+
+    if not more_levels_exist: 
+        player_reached_end_of_game()
+        return
+    else:
+        level_manager.instance_current_level()
