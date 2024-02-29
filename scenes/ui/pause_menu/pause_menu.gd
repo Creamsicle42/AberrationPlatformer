1f48304366ae3495f8eb6f37ecc487123166c5d3
Annabelle Lewis <defuego424242@gmail.com>
2024-02-29 12:59:16 -0800

    Created Pause Menu UI element

---
+++ scenes/ui/pause_menu/pause_menu.gd
@@ -0,0 +1,25 @@
+class_name PauseMenu
+extends CanvasLayer
+
+
+signal continue_pressed
+signal go_to_main_menu_pressed
+signal restart_level_pressed
+signal next_level_pressed
+
+
+
+func _on_mm_button_pressed() -> void:
+	go_to_main_menu_pressed.emit()
+
+
+func _on_next_button_pressed() -> void:
+	next_level_pressed.emit()
+
+
+func _on_restart_button_pressed() -> void:
+	restart_level_pressed.emit()
+
+
+func _on_continue_button_pressed() -> void:
+	continue_pressed.emit()
