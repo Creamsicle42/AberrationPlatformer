a236e47da65e57a9c475ed99bfc179b44968c7ef
Creamsicle <annabellelewis42@gmail.com>
2024-03-01 10:20:11 -0800

    Revert "v0.3"

--- assets/resources/_types/moving_platform_tween_target.gd
+++
@@ -1,8 +0,0 @@
-class_name MovingPlatformTweenTarget
-extends Resource
-
-
-@export var target : float
-@export var duration : float
-@export var trans_type : Tween.TransitionType
-@export var ease_type : Tween.EaseType
