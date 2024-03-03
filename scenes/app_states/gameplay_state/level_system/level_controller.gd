2086e36e30ae90175e599c5e7b8f3155b0562386
Annabelle Lewis <defuego424242@gmail.com>
2024-03-02 19:15:56 -0800

    Reworked level system to use spawnpoints

--- scenes/app_states/gameplay_state/level_system/level_controller.gd
+++ scenes/app_states/gameplay_state/level_system/level_controller.gd
@@ -2,3 +2,9 @@ class_name LevelController
 extends Node2D
 
 
+@export var player : Player
+@export var spawnpoints : Node
+
+
+func spawn_player_at_point(spawnpoint_id : String) -> void:
+	player.global_position = spawnpoints.get_node(spawnpoint_id.to_pascal_case()).global_position
--- scenes/app_states/main_menu_state/main_menu_app_state.gd
+++ scenes/app_states/main_menu_state/main_menu_app_state.gd
@@ -7,7 +7,7 @@ extends Node
 
 func enter_state(_params:Dictionary) -> void:
 	FadeLayer.fade_in()
-	if GameDataManager.current_game_data.level_on == -1:
+	if GameDataManager.current_game_data.level_on == "":
 		continue_button.disabled = true
 
 
@@ -19,7 +19,8 @@ func _on_new_game_button_pressed() -> void:
 	FadeLayer.fade_out()
 	await FadeLayer.fade_complete
 	Main.app.set_state("gameplay", {
-		"start_level": 0
+		"start_level": "test_1",
+		"start_spawnpoint": "test"
 	})
 
 
@@ -27,6 +28,7 @@ func _on_continue_button_pressed() -> void:
 	FadeLayer.fade_out()
 	await FadeLayer.fade_complete
 	Main.app.set_state("gameplay", {
-		"start_level": GameDataManager.current_game_data.level_on
+		"start_level": GameDataManager.current_game_data.level_on,
+		"start_spawnpoint": GameDataManager.current_game_data.checkpoint_on
 	})
 	
