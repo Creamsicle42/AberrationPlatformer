051e085a1ce215353ad21c3d7eb4aaf8b88d201b
Annabelle Lewis <defuego424242@gmail.com>
2024-03-06 20:33:21 -0800

    Added footstep sounds for all materials

--- scenes/player/footstep_manager.gd
+++ scenes/player/footstep_manager.gd
@@ -3,6 +3,8 @@ extends Node
 
 @export var foostep_player : AudioStreamPlayer
 @export var animation_player : AnimatedSprite2D
+@export var gravity_manager : GravityManager
+@export var sounds : Dictionary
 
 
 var footstep_anim := "run"
@@ -15,6 +17,23 @@ func _on_main_sprite_frame_changed() -> void:
     do_footstep()
 
 func do_footstep() -> void:
+    var world_tilemap : TileMap = get_tree().get_first_node_in_group("world_tilemap") as TileMap
+
+    var stream : AudioStream = sounds["stone"]
+
+    var foot_pos = owner.global_position + (gravity_manager.get_gravity_direction() * 12.0)
+
+    var grid_foot_pos = world_tilemap.local_to_map(foot_pos)
+
+    var foot_pos_data := world_tilemap.get_cell_tile_data(2, grid_foot_pos)
+
+    if foot_pos_data != null:
+
+        var sound_id = foot_pos_data.get_custom_data("footstep_sound")
+
+        stream = sounds.get(sound_id, stream)
+
+    foostep_player.stream = stream
     foostep_player.play()
 
 
