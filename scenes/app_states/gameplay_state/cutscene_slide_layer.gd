a92a4e67a3662e10b4a0b0f396cf8eeb2d2f4372
Annabelle Lewis <defuego424242@gmail.com>
2024-03-09 10:28:42 -0800

    Added slideshow manager for cutscenes

---
+++ scenes/app_states/gameplay_state/cutscene_slide_layer.gd
@@ -0,0 +1,15 @@
+class_name CutsceneSlideLayer
+extends CanvasLayer
+
+
+static var current : CutsceneSlideLayer 
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	current = self
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
