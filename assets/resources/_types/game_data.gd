1a9adcfe094f10f551f5970f9c579296631479bb
Annabelle Lewis <defuego424242@gmail.com>
2024-03-11 11:12:24 -0700

    Setup Fullscreening

--- assets/resources/_types/game_data.gd
+++ assets/resources/_types/game_data.gd
@@ -2,8 +2,9 @@ class_name GameData
 extends Resource
 
 
-@export var level_on : String = "sewers_1"
+@export var level_on : String = "intro"
 @export var checkpoint_on : String = "start"
 @export var flags : Dictionary = {}
 @export var music_volume : float = 0
 @export var sfx_volume : float = 0
+@export var fullscreen : bool = true
