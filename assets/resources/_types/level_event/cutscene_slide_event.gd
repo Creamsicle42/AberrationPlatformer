b6b89ea956ad70b77dba21dcb65ab602e3c74ca7
Annabelle Lewis <defuego424242@gmail.com>
2024-03-10 20:51:12 -0700

    Created Proper Cutscenes for ending

--- assets/resources/_types/level_event/cutscene_slide_event.gd
+++ assets/resources/_types/level_event/cutscene_slide_event.gd
@@ -5,8 +5,9 @@ extends LevelEvent
 @export var cutscene_slide : Texture2D
 @export_multiline var cutscene_text : String
 @export var slide_show_time : float
+@export var wait_for_accept := true
 
 
 func execute(_data : Dictionary) -> void:
     CutsceneSlideLayer.current.show_slide(cutscene_slide, cutscene_text, slide_show_time)
-    await CutsceneSlideLayer.current.text_accepted
+    if wait_for_accept: await CutsceneSlideLayer.current.text_accepted
