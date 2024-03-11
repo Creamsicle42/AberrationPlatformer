e4ccbcf3ad3e898b688634d8942fcfc78a1b86d7
Annabelle Lewis <defuego424242@gmail.com>
2024-03-11 13:23:45 -0700

    More bugfixes for web

--- scenes/ui/settings_pannel/settings_pannel.gd
+++ scenes/ui/settings_pannel/settings_pannel.gd
@@ -9,7 +9,7 @@ signal accept_pressed
 func _ready() -> void:
 	%MusicVolumeSlider.value = GameDataManager.current_game_data.music_volume
 	%SFXVolumeSlider.value = GameDataManager.current_game_data.sfx_volume
-	%FullscreenButton.button_pressed.visible = OS.has_feature("pc")
+	%FullscreenButton.visible = OS.has_feature("pc")
 	%FullscreenButton.button_pressed = GameDataManager.current_game_data.fullscreen
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("SFX"), GameDataManager.current_game_data.sfx_volume)
 	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), GameDataManager.current_game_data.music_volume)
@@ -32,5 +32,6 @@ func _on_music_volume_slider_value_changed(value:float) -> void:
 
 
 func _on_fullscreen_button_toggled(toggled_on:bool) -> void:
+	if not OS.has_feature("pc"): return
 	GameDataManager.current_game_data.fullscreen = toggled_on
 	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN if GameDataManager.current_game_data.fullscreen else DisplayServer.WINDOW_MODE_WINDOWED)
